58a115452825304cb3b033c2042108f09f657b21
BenjaminSchroeders <benjamin.schroeders@icloud.com>
2026-05-10 11:45:54 +0200

    Add PostgreSQL and Entity Framework setup

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TaskItem> Tasks { get; set; }
+}
